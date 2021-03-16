f815c0d08aa66444106a9275875b6d0e5c4a2a43
sadnessOjisan <sadness.ojisan@gmail.com>
2021-03-16 18:22:23 +0900

    fail by cors

--- README.md
+++ README.md
@@ -6,3 +6,19 @@ npm i
 
 npm run build
 ```
+
+## endpoints
+
+* https://3rd-party-script-git-main-ojisan.vercel.app/api/get?id=2
+
+### Client
+
+#### UI
+
+* https://3rd-party-script-git-main-ojisan.vercel.app/
+* https://3rd-party-script-git-main-ojisan.vercel.app/
+
+#### JS
+* https://3rd-party-script-git-main-ojisan.vercel.app/build.js
+* https://3rd-party-script-git-main-ojisan.vercel.app//bundle.js
+
--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,22 @@
 import { h, render } from "preact";
+import { useEffect, useState } from "preact/hooks";
 
 const App = () => {
-  return <div>hello app!</div>;
+  const [state, setSate] = useState<string | undefined>(undefined);
+
+  useEffect(() => {
+    const d = fetch(
+      " https://3rd-party-script-git-main-ojisan.vercel.app/api/get?id=2"
+    )
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        console.log(data);
+        setSate(data.id);
+      });
+  }, []);
+  return <div>hello {state}!</div>;
 };
 
 render(<App></App>, document.body);
